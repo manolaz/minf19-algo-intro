1a6163cbe05f3a173d9dfa3497f87b44dc4b6c6c
Tristan Nguyen <trungupdate@gmail.com>
2019-11-17 06:24:54 -0700

    feat: build exact list of dictionary

--- maze-resolver/maze_pathfinder.py
+++ maze-resolver/maze_pathfinder.py
@@ -1,26 +1,30 @@
 import os
 import io
 
-# algorithm start from here
-# 1.make graph diagram.txt
-
 def coordinate_parser(pos, len_col):
     p_row = pos//col
     p_col = pos%col
     return {"row": p_row, "col": p_col}
 
-def is_vertex(point, len_row):
-    position = point.get('position')
+def is_vertex(point, graph, len_row):
     neigbors = 0
-    pos_up = position - len_row
-    pos_down = position + len_row
-    pos_left = position - 1
-    pos_right = position + 1
+    pos_up = point - len_row
+    pos_down = point + len_row
+    pos_left = point - 1
+    pos_right = point + 1
 
-def is_point(pos, matrix):
-    neigbors = 0
-    is_point = False
-    return is_point
+    if graph.get(pos_up):
+        neigbors += 1
+    if graph.get(pos_down):
+        neigbors += 1
+    if graph.get(pos_left):
+        neigbors += 1
+    if graph.get(pos_right):
+        neigbors += 1
+    # this is not a Vertex only when having only 2 neigbors
+    if neigbors == 2:
+        return False
+    return True
 
 
 def edge_member(point, edge_list):
@@ -30,8 +34,8 @@ def edge_member(point, edge_list):
     else:
         return False
 
-def graph_serialiser(line, row_index, len_row):
-    ser_line = []
+def graph_serializer(line, row_index, len_row):
+    ser_line = {}
     init_col = 1
     for i in line:
         pos = row_index * len_row + init_col
@@ -40,23 +44,19 @@ def graph_serialiser(line, row_index, len_row):
             point = True
         else:
             point = False
-        point = {pos:point}
-        ser_line.append(point)
+        ser_line[pos] = point
         # increase the column index
         init_col += 1
     return ser_line
 
-
-# 2. Find the shortest path
 
 def graph_load():
     num_row = 0
     len_row = 0
     start_pos = -1
     exit_pos = -1
-    matrix = []
+    graph = {}
     file_path = input("File path :") + ".txt"
-    print(file_path)
     f = open(file_path, "r")
     row_index = 0
     maze_row = 0
@@ -64,12 +64,12 @@ def graph_load():
         line_data = line.rstrip()
         if row_index == 0 :
             num_row = int(line_data)
-            print(num_row)
         elif row_index == 1:
             len_row = int(line_data)
         elif row_index < num_row + 2:
-            row = graph_serialiser(line_data, maze_row, len_row)
-            matrix.append(row)
+            row = graph_serializer(line_data, maze_row, len_row)
+            # graph.extend(row)
+            graph.update(row)
             maze_row += 1
         elif row_index == num_row + 2:
             start_pos = int (line_data)
@@ -77,23 +77,26 @@ def graph_load():
             exit_pos = int (line_data)
         row_index += 1
     f.close()
-    return {"num_row" :num_row,
-    "len_row": len_row,
-    "start_pos": start_pos,
-    "exit_pos": exit_pos,
-    "graph": matrix}
+    return {
+            "num_row" :num_row,
+            "len_row": len_row,
+            "start_pos": start_pos,
+            "exit_pos": exit_pos,
+            "graph": graph
+            }
 
 
 def main():
-    # Get data
     file = graph_load()
     graph = file.get("graph")
-    print(file.get("num_row"))
-    print(file.get("len_row"))
-    print(file.get("start_pos"))
-    print(file.get("exit_pos"))
-    for line in graph:
-        print(line)
-
+    g_height = (file.get("num_row"))
+    g_width = (file.get("len_row"))
+    source = (file.get("start_pos"))
+    target = (file.get("exit_pos"))
+    edge_list = []
+    for point in graph.keys():
+        if is_vertex(point, graph, g_width):
+            edge_list.append(point)
+            print("position {}, on path ? = {}".format(point, graph[point]))
 if __name__ == '__main__':
     main()
