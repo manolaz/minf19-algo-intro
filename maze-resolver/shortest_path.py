4d9ad23f793f58f4554d87620922108fb5775a63
Meow <hanh.vo.programmer@gmail.com>
2019-11-19 10:34:08 +0700

    shortest_path

---
+++ maze-resolver/shortest_path.py
@@ -0,0 +1,190 @@
+import sys
+
+class Vertex:
+    def __init__(self, node):
+        self.id = node
+        self.adjacent = {}
+        # Set distance to infinity for all nodes
+        self.distance = sys.maxint
+        # Mark all nodes unvisited        
+        self.visited = False  
+        # Predecessor
+        self.previous = None
+
+    def add_neighbor(self, neighbor, weight=0):
+        self.adjacent[neighbor] = weight
+
+    def get_connections(self):
+        return self.adjacent.keys()  
+
+    def get_id(self):
+        return self.id
+
+    def get_weight(self, neighbor):
+        return self.adjacent[neighbor]
+
+    def set_distance(self, dist):
+        self.distance = dist
+
+    def get_distance(self):
+        return self.distance
+
+    def set_previous(self, prev):
+        self.previous = prev
+
+    def set_visited(self):
+        self.visited = True
+
+    def __str__(self):
+        return str(self.id) + ' adjacent: ' + str([x.id for x in self.adjacent])
+
+class Graph:
+    def __init__(self):
+        self.vert_dict = {}
+        self.num_vertices = 0
+
+    def __iter__(self):
+        return iter(self.vert_dict.values())
+
+    def add_vertex(self, node):
+        self.num_vertices = self.num_vertices + 1
+        new_vertex = Vertex(node)
+        self.vert_dict[node] = new_vertex
+        return new_vertex
+
+    def get_vertex(self, n):
+        if n in self.vert_dict:
+            return self.vert_dict[n]
+        else:
+            return None
+
+    def add_edge(self, frm, to, cost = 0):
+        if frm not in self.vert_dict:
+            self.add_vertex(frm)
+        if to not in self.vert_dict:
+            self.add_vertex(to)
+
+        self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
+        self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
+
+    def get_vertices(self):
+        return self.vert_dict.keys()
+
+    def set_previous(self, current):
+        self.previous = current
+
+    def get_previous(self, current):
+        return self.previous
+
+def shortest(v, path):
+    ''' make shortest path from v.previous'''
+    if v.previous:
+        path.append(v.previous.get_id())
+        shortest(v.previous, path)
+    return
+
+import heapq
+
+def dijkstra(aGraph, start):
+    print '''Dijkstra's shortest path'''
+    # Set the distance for the start node to zero 
+    start.set_distance(0)
+
+    # Put tuple pair into the priority queue
+    unvisited_queue = [(v.get_distance(),v) for v in aGraph]
+    heapq.heapify(unvisited_queue)
+
+    while len(unvisited_queue):
+        # Pops a vertex with the smallest distance 
+        uv = heapq.heappop(unvisited_queue)
+        current = uv[1]
+        current.set_visited()
+
+        #for next in v.adjacent:
+        for next in current.adjacent:
+            # if visited, skip
+            if next.visited:
+                continue
+            new_dist = current.get_distance() + current.get_weight(next)
+            
+            if new_dist < next.get_distance():
+                next.set_distance(new_dist)
+                next.set_previous(current)
+                print 'updated : current = %s next = %s new_dist = %s' \
+                        %(current.get_id(), next.get_id(), next.get_distance())
+            else:
+                print 'not updated : current = %s next = %s new_dist = %s' \
+                        %(current.get_id(), next.get_id(), next.get_distance())
+
+        # Rebuild heap
+        # 1. Pop every item
+        while len(unvisited_queue):
+            heapq.heappop(unvisited_queue)
+        # 2. Put all vertices not visited into the queue
+        unvisited_queue = [(v.get_distance(),v) for v in aGraph if not v.visited]
+        heapq.heapify(unvisited_queue)
+
+
+if __name__ == '__main__':
+
+    g = Graph()
+
+    # 1(17,2)
+# 17(1,2)(21,4)(33,2)
+# 21(17,4)(22,1)(52,5)
+# 22(21,1)
+# 33(17,2)(34,1)(49,2)
+# 34(33,1)
+# 49(33,2)(52,3)(76,6)
+# 52(21,5)(49,3)(55,3)
+# 55(52,3)
+# 68(76,1)
+# 76(49,6)(68,1)(78,2)
+# 78(76,2)
+
+
+    # Add list vertices 
+
+    g.add_vertex('1')
+    g.add_vertex('17')
+    g.add_vertex('21')
+    g.add_vertex('22')
+    g.add_vertex('52')
+    g.add_vertex('49')
+    g.add_vertex('76')
+    g.add_vertex('68')
+    g.add_vertex('78')
+    g.add_vertex('33')
+    g.add_vertex('34')
+    g.add_vertex('55')
+
+    # Add list edge 
+    g.add_edge('1', '17', 2)  
+    g.add_edge('21', '17', 4)
+    g.add_edge('21', '22', 1)
+    g.add_edge('21', '52', 5)
+    g.add_edge('49', '33', 2)
+    g.add_edge('49', '76', 6)
+    g.add_edge('68', '76', 1)
+    g.add_edge('76', '78', 6)
+    g.add_edge('33', '17', 2)
+    g.add_edge('33', '34', 1)
+    g.add_edge('33', '49', 2)
+    g.add_edge('49', '52', 3)
+    g.add_edge('52', '55', 3)
+    g.add_edge('76', '49', 6)
+
+    print 'Graph data:'
+    for v in g:
+        for w in v.get_connections():
+            vid = v.get_id()
+            wid = w.get_id()
+            print '( %s , %s, %3d)'  % ( vid, wid, v.get_weight(w))
+
+            
+    dijkstra(g, g.get_vertex('17')) 
+
+    target = g.get_vertex('55')
+    path = [target.get_id()]
+    shortest(target, path)
+    print 'The shortest path : %s' %(path[::-1])
